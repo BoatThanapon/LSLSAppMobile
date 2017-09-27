ea57c4fffabcd8347208a3d485692f894a2909f0
THANAPON CHAPCHAINAI <thanapon_chap@cmu.ac.th>
2017-09-27 15:35:17 +0700

    Google Map Api is implemented

--- app/src/main/java/com/example/bearboat/lslsapp/fragment/MapsFragment.java
+++ app/src/main/java/com/example/bearboat/lslsapp/fragment/MapsFragment.java
@@ -7,9 +7,18 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.bearboat.lslsapp.R;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsFragment extends Fragment{
-
+    MapView mMapView;
+    private GoogleMap googleMap;
     public static MapsFragment newInstance() {
         MapsFragment fragment = new MapsFragment();
         return fragment;
@@ -21,13 +30,50 @@ public class MapsFragment extends Fragment{
         View rootView = inflater.inflate(R.layout.fragment_maps, container, false);
         initInstances(rootView);
 
+        mMapView.onCreate(savedInstanceState);
+
+        onMapSetup();
+
         return rootView;
 
 
     }
 
-    private void initInstances(View rootView) {
+    private void onMapSetup() {
+        mMapView.onResume();// needed to get the map to display immediately
 
+        try {
+            MapsInitializer.initialize(getActivity().getApplicationContext());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        googleMap = mMapView.getMap();
+        // latitude and longitude
+        double latitude = 17.385044;
+        double longitude = 78.486671;
+
+        // create marker
+        MarkerOptions marker = new MarkerOptions().position(
+                new LatLng(latitude, longitude)).title("Hello Maps");
+
+        // Changing marker icon
+        marker.icon(BitmapDescriptorFactory
+                .defaultMarker(BitmapDescriptorFactory.HUE_ROSE));
+
+        // adding marker
+        googleMap.addMarker(marker);
+        CameraPosition cameraPosition = new CameraPosition.Builder()
+                .target(new LatLng(17.385044, 78.486671)).zoom(12).build();
+        googleMap.animateCamera(CameraUpdateFactory
+                .newCameraPosition(cameraPosition));
+
+        googleMap.setMyLocationEnabled(true);
+
+    }
+
+    private void initInstances(View rootView) {
+        mMapView = (MapView) rootView.findViewById(R.id.mapView);
     }
 
     /*
@@ -56,5 +102,28 @@ public class MapsFragment extends Fragment{
         }
     }
 
+    @Override
+    public void onResume() {
+        super.onResume();
+        mMapView.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mMapView.onPause();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mMapView.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mMapView.onLowMemory();
+    }
 
 }
